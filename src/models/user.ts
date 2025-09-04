bc6c00fd68161eb1c13619f5f8a65b222451a82f
samstringzz <freetlessguitarist@gmail.com>
2025-09-04 20:15:11 +0100

    users

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,6 +1,6 @@
 import pool from '../config/database';
 import { User, CreateUserDTO, UserProfile } from '../types/user';
-import bcrypt from 'bcrypt';i mean
+import bcrypt from 'bcrypt';
 
 export class UserModel {
   static async create(userData: CreateUserDTO): Promise<UserProfile> {
